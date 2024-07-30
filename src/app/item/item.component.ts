0f123ce2bc3a339bb7e6f82c5787e72a563a541e
Luis Miguel <oluismrs@gmail.com>
2024-07-30 20:55:56 -0300

    migrate logic to service based and add material design 3 components

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,24 +1,37 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Item } from "../../types";
+import { ItemService } from "./item.service";
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Item } from "../item";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css'
+  styleUrl: './item.component.css',
+  imports: [CommonModule, MatCheckboxModule, MatButtonModule, MatIconModule],
 })
 export class ItemComponent {
-  editable = false;
+  itemService = inject(ItemService)
 
   @Input() item!: Item;
-  @Output() remove = new EventEmitter<Item>();
 
-  saveItem(description: string) {
-    if (!description) return;
+  isEditable = false
 
-    this.editable = false;
-    this.item.description = description;
+  updateItemState() {
+    this.item.done = !this.item.done
+    this.itemService.updateItem(this.item)
+  }
+
+  updateItemDesc(description: string) {
+    this.isEditable = false
+    this.item.description = description
+    this.itemService.updateItem(this.item)
+  }
+
+  removeItem() {
+    this.itemService.removeItem(this.item.id)
   }
 }
